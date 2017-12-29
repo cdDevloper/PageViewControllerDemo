3845750d78d0f2d4802e21c49ebd17e5bdb67aba
Bhagwant-MacBook <bhagwant-macbook@Bhagwant-M.local>
2017-12-30 01:48:10 +0530

    IntroductionScreen Implementation

---
+++ PageBasedDemo/DataViewController.swift
@@ -0,0 +1,60 @@
+//
+//  DataViewController.swift
+//  PageBasedDemo
+//
+//  Created by Bhagwant-MacBook on 01/01/01.
+//  Copyright © 2001 CDDevloper. All rights reserved.
+//
+
+import UIKit
+
+protocol DataViewControllerDelegate{
+    func moveToNextViewController(index:Int)
+}
+
+class DataViewController: UIViewController {
+
+    @IBOutlet
+    weak var mainTitle: UILabel!
+    @IBOutlet
+    weak var subTitle:  UILabel!
+    
+    @IBOutlet
+    weak var imgView:   UIImageView!
+    
+    @IBOutlet
+    weak var btnNext:   UIButton!
+    
+    var mainTitleData:  String = ""
+    var subTitleData:   String = ""
+    var imgName:        String = ""
+    var btnTitle:       String = ""
+    var index:          Int = 0
+    
+    var delegate:DataViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.mainTitle.text = mainTitleData
+        self.subTitle.text  = subTitleData
+        self.imgView.image  = UIImage(named: imgName)
+        self.btnNext.setTitle(btnTitle, for: .normal)
+        
+        if index == 0{
+          self.view.backgroundColor = UIColor(red: 61.0/255.0, green: 255.0/255.0, blue: 237.0/255.0, alpha: 1.0)
+        }else{
+            self.view.backgroundColor = UIColor.red
+        }
+    }
+
+    @IBAction func btnNextPress(_ sender: Any) {
+        self.delegate?.moveToNextViewController(index: index)
+    }
+
+}
+
---
+++ PageBasedDemo/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  PageBasedDemo
+//
+//  Created by Bhagwant-MacBook on 01/01/01.
+//  Copyright © 2001 CDDevloper. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
